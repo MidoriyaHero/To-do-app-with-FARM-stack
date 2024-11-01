b7db2bc37a849fe575c7a7fe0516b89019246b88
MidoriyaHero <buitrungtin0105@gmail.com>
2024-11-01 17:38:56 +0700

    add router todo+ todo schema

--- app/api/handler/todo.py
+++ app/api/handler/todo.py
@@ -1,3 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.models.user_model import User
+from app.api.dependencies.user_dependency import get_current_user
+from app.schemas.todo_schema import TodoOut
+
 
 todo_router = APIRouter()
+@todo_router.get('/', response_model = TodoOut)
+async def list(current_user: User = Depends(get_current_user)):
+    pass
---
+++ app/schemas/todo_schema.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from uuid import UUID
+from datetime import datetime
+
+class TodoCreate(BaseModel):
+    title: str = Field(..., title = "Title")
+    description: str = Field(..., title= "Description")
+    status: Optional[bool] = False
+
+class TodoUpdate(BaseModel):
+    title: Optional[str] = Field(..., title = "Title")
+    description: Optional[str] = Field(..., title= "Description")
+    status: Optional[bool] = False
+
+class TodoOut(BaseModel):
+    todo_id: UUID
+    status: bool
+    title: str
+    description: str
+    create_at: datetime
+    update_at: datetime
+    
